f80559d2995bd3d66cb469eef39f646161b87e8d
Daniel Kocev <danielkocev.it@gmail.com>
2022-07-09 22:43:10 +0200

    add buttons and labels for taps

--- TV Show/TV Show/Login/LoginViewController.swift
+++ TV Show/TV Show/Login/LoginViewController.swift
@@ -4,5 +4,36 @@
 //
 //  Created by infinum on 08/07/2022.
 //
+//
+//
+import UIKit
 
-import Foundation
+class LoginViewController: UIViewController {
+//
+    
+    @IBOutlet weak var myViewController: UIView!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var viewNumberOfTaps: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+        myViewController.backgroundColor = .purple
+        
+
+    }
+    var tapsCounter = 0
+    @IBAction func countNumberOfTaps(){
+        tapsCounter += 1
+        viewNumberOfTaps.text = String(format: "%d", tapsCounter)
+        
+    }
+    
+    
+    @IBAction func resetCounter(){
+        tapsCounter = 0
+        viewNumberOfTaps.text = String(format: "%d", tapsCounter)
+    }
+
+}
+
