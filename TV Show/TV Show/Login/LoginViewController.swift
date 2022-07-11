e4b0562a9aafa3a075ffe522d3fb22c13b8be32b
Daniel Kocev <danielkocev.it@gmail.com>
2022-07-11 16:02:33 +0200

    added activity indicator, start/hide on start after 3 seconds and after button is clicked

--- TV Show/TV Show/Login/LoginViewController.swift
+++ TV Show/TV Show/Login/LoginViewController.swift
@@ -14,24 +14,28 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var myViewController: UIView!
     @IBOutlet weak var button: UIButton!
     @IBOutlet weak var viewNumberOfTaps: UILabel!
- 
     @IBOutlet weak var doRoundedButton: UIButton!
-    
     @IBOutlet var myRoundedButtons: [UIButton]!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
       // Do any additional setup after loading the view.
-      //  myViewController.backgroundColor = .purple
+      
         roundedButtons()
-        button.imageView?.contentMode = .scaleAspectFit
-        
-                   
-                
+       
+        // Call function to start activity indicator
+        activityIndicatorStarted()
+        // Call function to stop activity indicator after 3 seconds
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+            self.activityIndicatorStopped()
+         }
+               
 
     }
   
-        
+    // create edges around buttons
     func roundedButtons(){
         for button in myRoundedButtons {
             button.layer.cornerRadius = 15.0
@@ -40,11 +44,36 @@ class LoginViewController: UIViewController {
         }
     }
     
+    // Section for Activity Indicator
+    var isActivityIndicatorPressed = true
     
+    func activityIndicatorStarted(){
+        activityIndicator.startAnimating()
+        isActivityIndicatorPressed = false
+    }
+    
+    func activityIndicatorStopped(){
+        activityIndicator.stopAnimating()
+        activityIndicator.hidesWhenStopped = true
+        isActivityIndicatorPressed = true
+    }
+    
+    // Show/Hide activity indicator when button is clicked
+    func activityIndicatorPressed(){
+        if isActivityIndicatorPressed{
+            activityIndicatorStarted()
+        } else {
+            activityIndicatorStopped()
+        }
+        
+    }
+    
+    // Section for events when buttons are clicked
     var tapsCounter = 0
     @IBAction func countNumberOfTaps(){
         tapsCounter += 1
         viewNumberOfTaps.text = String(format: "%d", tapsCounter)
+        activityIndicatorPressed()
         
     }
     
@@ -52,6 +81,7 @@ class LoginViewController: UIViewController {
     @IBAction func resetCounter(){
         tapsCounter = 0
         viewNumberOfTaps.text = String(format: "%d", tapsCounter)
+        activityIndicatorStopped()
     }
     
    
