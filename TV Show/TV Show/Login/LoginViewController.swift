e7530348ebdde41f56a59bd0cf22ff682ab530c7
Daniel Kocev <danielkocev.it@gmail.com>
2022-07-12 14:15:12 +0200

    code is restructured by MARK sorting.

--- TV Show/TV Show/Login/LoginViewController.swift
+++ TV Show/TV Show/Login/LoginViewController.swift
@@ -8,83 +8,104 @@
 //
 import UIKit
 
-class LoginViewController: UIViewController {
-//
+final class LoginViewController: UIViewController {
     
-    @IBOutlet weak var myViewController: UIView!
-    @IBOutlet weak var button: UIButton!
-    @IBOutlet weak var viewNumberOfTaps: UILabel!
-    @IBOutlet weak var doRoundedButton: UIButton!
-    @IBOutlet var myRoundedButtons: [UIButton]!
-    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    // MARK: - Outlets
     
+    @IBOutlet private weak var myViewController: UIView!
+    @IBOutlet private weak var button: UIButton!
+    @IBOutlet private weak var viewNumberOfTaps: UILabel!
+    @IBOutlet private weak var doRoundedButton: UIButton!
+    @IBOutlet private var myRoundedButtons: [UIButton]!
+    @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
+    
+    //MARK: - Properties
+    
+    private var tapsCounter = 0
+    private var isActivityIndicatorPressed = true
+    
+    // MARK: - Lifecycle methods
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
       // Do any additional setup after loading the view.
       
         roundedButtons()
        
         // Call function to start activity indicator
+        
         activityIndicatorStarted()
+        
         // Call function to stop activity indicator after 3 seconds
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
-            self.activityIndicatorStopped()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) { [weak self] in
+            self?.activityIndicatorStopped()
+            
          }
-               
-
     }
   
-    // create edges around buttons
-    func roundedButtons(){
-        for button in myRoundedButtons {
-            button.layer.cornerRadius = 15.0
-            button.layer.masksToBounds = true
+    // MARK: - Actions
+    
+    @IBAction private func countNumberOfTaps(){
         
-        }
-    }
-    
-    // Section for Activity Indicator
-    var isActivityIndicatorPressed = true
-    
-    func activityIndicatorStarted(){
-        activityIndicator.startAnimating()
-        isActivityIndicatorPressed = false
-    }
-    
-    func activityIndicatorStopped(){
-        activityIndicator.stopAnimating()
-        activityIndicator.hidesWhenStopped = true
-        isActivityIndicatorPressed = true
-    }
-    
-    // Show/Hide activity indicator when button is clicked
-    func activityIndicatorPressed(){
-        if isActivityIndicatorPressed{
-            activityIndicatorStarted()
-        } else {
-            activityIndicatorStopped()
-        }
-        
-    }
-    
-    // Section for events when buttons are clicked
-    var tapsCounter = 0
-    @IBAction func countNumberOfTaps(){
         tapsCounter += 1
         viewNumberOfTaps.text = String(format: "%d", tapsCounter)
         activityIndicatorPressed()
         
     }
     
-    
-    @IBAction func resetCounter(){
+    @IBAction private func resetCounter(){
+        
         tapsCounter = 0
         viewNumberOfTaps.text = String(format: "%d", tapsCounter)
         activityIndicatorStopped()
+        
     }
     
-   
-
+    //MARK: Utility methods
+    
+    // create edges around buttons
+    
+    private func roundedButtons(){
+        
+        for button in myRoundedButtons {
+            
+            button.layer.cornerRadius = 15.0
+            button.layer.masksToBounds = true
+            
+        }
+    }
+    
+    // Section for Activity Indicator when is started or stopped
+    
+    private func activityIndicatorStarted(){
+        
+        activityIndicator.startAnimating()
+        isActivityIndicatorPressed = false
+        
+    }
+    
+    private func activityIndicatorStopped(){
+        
+        activityIndicator.stopAnimating()
+        activityIndicator.hidesWhenStopped = true
+        isActivityIndicatorPressed = true
+        
+    }
+    
+    // Show-Hide activity indicator when button is clicked
+    
+    private func activityIndicatorPressed(){
+        
+        if isActivityIndicatorPressed{
+            
+            activityIndicatorStarted()
+            
+        } else {
+            
+            activityIndicatorStopped()
+            
+        }
+    }
 }
-
