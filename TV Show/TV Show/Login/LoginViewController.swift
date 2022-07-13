038d9efcf4d2ebc78487247a7fb04d632f3175af
Daniel Kocev <danielkocev.it@gmail.com>
2022-07-13 16:34:10 +0200

    added purple background, top-left/right splash image

--- TV Show/TV Show/Login/LoginViewController.swift
+++ TV Show/TV Show/Login/LoginViewController.swift
@@ -7,23 +7,17 @@
 //
 //
 import UIKit
+import MBProgressHUD
 
 final class LoginViewController: UIViewController {
     
     // MARK: - Outlets
     
-    @IBOutlet private weak var myViewController: UIView!
-    @IBOutlet private weak var button: UIButton!
-    @IBOutlet private weak var viewNumberOfTaps: UILabel!
-    @IBOutlet private weak var doRoundedButton: UIButton!
-    @IBOutlet private var myRoundedButtons: [UIButton]!
-    @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
+   
     
     //MARK: - Properties
     
-    private var tapsCounter = 0
-    private var isActivityIndicatorPressed = true
-    
+   
     // MARK: - Lifecycle methods
     
     override func viewDidLoad() {
@@ -31,82 +25,19 @@ final class LoginViewController: UIViewController {
         super.viewDidLoad()
         
       // Do any additional setup after loading the view.
-      
-        roundedButtons()
-       
-        // Call function to start activity indicator
+      // MBProgressHUD.showAdded(to: view, animated: true)
         
-        activityIndicatorStarted()
+        // Backround color is #52368C
         
-        // Call function to stop activity indicator after 3 seconds
+        self.view.backgroundColor = UIColor(red: 82/250.0, green: 54/250.0, blue: 140/250.0, alpha: 1)
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) { [weak self] in
-            self?.activityIndicatorStopped()
-            
-         }
     }
   
     // MARK: - Actions
     
-    @IBAction private func countNumberOfTaps(){
-        
-        tapsCounter += 1
-        viewNumberOfTaps.text = String(format: "%d", tapsCounter)
-        activityIndicatorPressed()
-        
-    }
-    
-    @IBAction private func resetCounter(){
-        
-        tapsCounter = 0
-        viewNumberOfTaps.text = String(format: "%d", tapsCounter)
-        activityIndicatorStopped()
-        
-    }
     
     //MARK: Utility methods
     
-    // create edges around buttons
+   
     
-    private func roundedButtons(){
-        
-        for button in myRoundedButtons {
-            
-            button.layer.cornerRadius = 15.0
-            button.layer.masksToBounds = true
-            
-        }
-    }
-    
-    // Section for Activity Indicator when is started or stopped
-    
-    private func activityIndicatorStarted(){
-        
-        activityIndicator.startAnimating()
-        isActivityIndicatorPressed = false
-        
-    }
-    
-    private func activityIndicatorStopped(){
-        
-        activityIndicator.stopAnimating()
-        activityIndicator.hidesWhenStopped = true
-        isActivityIndicatorPressed = true
-        
-    }
-    
-    // Show-Hide activity indicator when button is clicked
-    
-    private func activityIndicatorPressed(){
-        
-        if isActivityIndicatorPressed{
-            
-            activityIndicatorStarted()
-            
-        } else {
-            
-            activityIndicatorStopped()
-            
-        }
-    }
 }
